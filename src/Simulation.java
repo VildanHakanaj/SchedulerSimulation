f68b9914147c7ae04da14e24ea3b0027a7d0c931
Joshua <joshuacroft@trentu.ca>
2018-10-10 17:57:36 -0400

    -Added gaussian methods -Added generation class -Added comments to jobSet method

--- src/Simulation.java
+++ src/Simulation.java
@@ -13,15 +13,30 @@ class Simulation {
     }
 
     private void createJobSet1() {
+        int mean, stdDev;
+        mean = 150;
+        stdDev = 20;
+
         for (int i = 0; i < 10; i++) {
             Job job = new Job(20, 01);
         }
     }
     private void createJobSet2() {
+        int mean, stdDev;
+        //make job
+        //set job type
+        /*set mean/stdDev based on job type
+        if() {
+
+        } else {
+
+        }
+        */
 
     }
 
     private void createJobSet3() {
+        int mean, stdDev;
 
     }
 }
