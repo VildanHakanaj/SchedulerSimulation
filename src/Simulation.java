7b6a695e700372b30e0fe3d3d49aabdcc60b3548
Vildan Hakanaj <vildanhakanaj@gmail.com>
2018-10-18 16:44:20 -0400

    Started the Shortes Job first alogrithm

--- src/Simulation.java
+++ src/Simulation.java
@@ -8,7 +8,9 @@ class Simulation {
     private double responseTime = 0.0; //Response time
     int contextSwitch = 0;
     private int arrivalTime = 0; //The first arrival time
+    private Job currentJob; //Store the job
 
+    //Constant Declarations
     private final int MEAN_ARRIVAL = 160;
     private final int STANDARD_DEVIATION_ARRIVAL = 15;
     private final int MEAN_JOB_SET_ONE = 150;
@@ -17,9 +19,10 @@ class Simulation {
     private final int STANDARD_DEVIATION_JOB_SMALL = 5;
     private final int MEAN_JOB_SET_TWO_LARGE = 250;         // 20% of jobs in set 2, 80% of jobs in set 3
     private final int STANDARD_DEVIATION_JOB_LARGE = 15;
+    //End of constant declaration block
     private ArrayList<Job> jobs; //Store the intial jobs
-//    private Queue<Job> queue = new LinkedList<>();
-//    private PriorityQueue<Job> pq = new PriorityQueue<>(); //Using a priority Queue to sort he job based on the arrival time.
+
+
     Simulation() {
         this.random = new Random(); //Create the new randoms
     }
@@ -28,7 +31,7 @@ class Simulation {
     void runSimulation() {
         jobs = createJobSet1();
         System.out.println("Before the First in first out");
-        runFcfs(jobs); //Run the algorithm of first come first serve
+        runFCFS(jobs); //Run the algorithm of first come first serve
     }
     /*
      * Create the job set 1
@@ -111,28 +114,57 @@ class Simulation {
     * First Come First Serve
     * The first Job that comes will be served and done with.
     *  */
-    public void runFcfs(ArrayList<Job> jobList){
-        Job currentJob; //Store the job
+    public void runFCFS(ArrayList<Job> jobList){
+        resetVar();
         currentJob = jobList.get(0); //Get the first Job.
         jobList.remove(0); //Remove the first job that has been processed
         clock = currentJob.getJobLength() + currentJob.getArrivalTime(); //Update the first clock time
-
         while(!jobList.isEmpty()){ //Go through the list until all there is no more jobs
-            for (int i = 0; i < jobList.size(); i++) //Loop to find the next job that arrives.
-                if(jobList.get(i).getArrivalTime() <= clock){ //if the Job has arrived
+            for (int i = 0; i < jobList.size(); i++) { //Loop to find the next job that arrives.
+                if (jobList.get(i).getArrivalTime() <= clock) { //if the Job has arrived
                     currentJob = jobList.get(i); //Get the job from the list
                     jobList.remove(i); //Then remove it.
                     break;
                 }
-            System.out.println("Job " + currentJob.getJobId() + " has arrived at " + currentJob.getArrivalTime() + " | and started processing at: " + clock);
+            }
+//            System.out.println("Job " + currentJob.getJobId() + " has arrived at " + currentJob.getArrivalTime() + " | and started processing at: " + clock);
             responseTime += clock -  currentJob.getArrivalTime(); //Add the respons time.
             clock += currentJob.getJobLength();//Run the process.
             System.out.println("Job: " + currentJob.getJobId() + " has finished processing at: " + clock);
         }
-        System.out.println("The response time for FCFS is: " + responseTime / NUM_TRIALS);
+//        System.out.println("The response time for FCFS is: " + responseTime / NUM_TRIALS);
     }
 
-    public void sJf(){
-
+    public void runSJF(ArrayList<Job> jobList){
+        resetVar();
+        ArrayList<Job> arrivedJobs = new ArrayList<>();
+        currentJob = jobList.get(0);
+        jobList.remove(0);
+        clock = currentJob.getJobLength() + currentJob.getArrivalTime(); //Update the clock for the first job;
+        while(jobList.size() > 0){
+            for(int i = 0; i < jobList.size(); i++){
+                if(jobList.get(i).getArrivalTime() < clock){ //Loop and find all the arrived jobs;
+                    arrivedJobs.add(jobList.get(i)); //Add the jobs that have arrived
+                    jobList.remove(i);//Remove the job from the list
+                }
+            }
+            Collections.sort(arrivedJobs);//Sort the list so that the shortest job is next;
+            currentJob = arrivedJobs.get(0);
+            responseTime += clock - currentJob.getArrivalTime();
+            clock += currentJob.getJobLength();
+        }
     }
+    /****************************************
+     *           Assitant methods
+     **************************************
+     */
+    /*Reset all the variables to start state of the schedule*/
+    private void resetVar(){
+        clock = 0;
+        responseTime = 0;
+        currentJob = null;
+        contextSwitch = 0;
+    }
+
+
 }
